6c67150901eb9f9bf028287914369442ff820365
Мирослав <101458707+mirik297@users.noreply.github.com>
2022-12-16 00:51:36 +0200

    Test realization

---
+++ BLL.Tests/EmergencyServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using BLL.Services.Implementations;
+using BLL.Services.Interfaces;
+using CCL.Security;
+using CCL.Security.Identity;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BLL.Tests
+{
+    public class EmergencyServiceTest
+    {
+        [Fact]
+        public void Ctor_InputNull_ThrowArgumentNullException()
+        {
+            // Arrange
+            IUnitOfWork nullUnitOfWork = null;
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new EmergencyService(nullUnitOfWork));
+        }
+
+        [Fact]
+        public void GetEmergency_FromDAL_CorrectMappingToDTO()
+        {
+            var emergencyService = GetEmergencyService();
+
+            var actual = emergencyService.GetEmergencies(1,0).First();
+
+            Assert.True(
+                actual.StreetId == 1
+                && actual.Id == 2
+                && actual.House == 2
+                && actual.TypeId == 1
+                );
+        }
+
+        IEmergencyService GetEmergencyService()
+        {
+            var mockContext = new Mock<IUnitOfWork>();
+            var expectedEmergency = new Emergency() { Id = 2, Date = new DateOnly(), StreetId = 1, House = 2, TypeId = 1};
+            var mockDbSet = new Mock<IEmergencyRepository>();
+            mockDbSet.Setup(e =>e.Find(It.IsAny<Func<Emergency, bool>>(),It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(
+                    new List<Emergency>() { expectedEmergency }
+                    );
+            mockContext.Setup(context => context.Emergencies).Returns(mockDbSet.Object);
+
+            IEmergencyService emergencyService = new EmergencyService(mockContext.Object);
+
+            return emergencyService;
+        }
+    }
+}
