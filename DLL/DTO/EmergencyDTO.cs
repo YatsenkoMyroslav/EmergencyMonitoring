77250820fcea8118febe43aaae6fb2d57c134cf7
Мирослав <101458707+mirik297@users.noreply.github.com>
2022-12-15 23:30:12 +0200

    DTO added

---
+++ DLL/DTO/EmergencyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.DTO
+{
+    public class EmergencyDTO
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public int StreetId { get; set; }
+        public int House { get; set; }
+        public int TypeId { get; set; }
+    }
+}
