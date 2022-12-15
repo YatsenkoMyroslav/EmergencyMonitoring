6e482b44abe2960c5dbf971a24b985feca1649b6
Мирослав <101458707+mirik297@users.noreply.github.com>
2022-12-16 00:11:38 +0200

    DLL realization

---
+++ DLL/Services/Implementations/EmergencyService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CCL.Security;
+using CCL.Security.Identity;
+using DAL.Entities;
+using DAL.UnitOfWork;
+using DLL.DTO;
+using DLL.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Services.Implementations
+{
+    public class EmergencyService:IEmergencyService
+    {
+        private readonly IUnitOfWork _database;
+        private int amountOnPage = 10;
+
+        public EmergencyService(IUnitOfWork unitOfWork)
+        {
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException(nameof(unitOfWork));
+            }
+            _database = unitOfWork;
+        }
+
+        public IEnumerable<EmergencyDTO> GetEmergencies(int emergencyTypeId, int page)
+        {
+            var emergenciesEntities = _database.Emergencies.Find(e => e.TypeId == emergencyTypeId, page, amountOnPage);
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Emergency, EmergencyDTO>()).CreateMapper();
+            var emeergencies = mapper.Map<IEnumerable<Emergency>, List<EmergencyDTO>>(emergenciesEntities);
+            
+            return emeergencies;
+        }
+    }
+}
---
+++ DLL/Services/Interfaces/IEmergencyService.cs
@@ -0,0 +1,14 @@
+﻿using DLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Services.Interfaces
+{
+    public interface IEmergencyService
+    {
+        IEnumerable<EmergencyDTO> GetEmergencies(int emergencyTypeId, int page);
+    }
+}
